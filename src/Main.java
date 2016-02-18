b73bd74c1b784a49aecaaed4e2a4e8f856692a1e
martithi <martithi@PC-B123-14.iut-valence.fr>
2016-02-18 12:01:12 +0100

    Create a game with play()
    -
    -

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main
+{
+
+	public static void main(String[] args)
+	{
+		Game sokoban = new sokoban.play();
+		System.out.println("La partie a commencé.");
+		
+		
+		/*Caisse caisse1 = new Caisse();	// Box creation
+		Caisse caisse2 = new Caisse();	// Box creation
+		*/
+	}
+
+}
